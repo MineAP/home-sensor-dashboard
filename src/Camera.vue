f6abfd0efe56e1401ee0e7cb5ac6ce0c57ae1887
shiramine keisuke <sira0925@gmail.com>
2023-07-19 18:50:33 +0900

    温度履歴画面のUI改善、カメラ画面に姿勢推定処理追加

--- src/Camera.vue
+++ src/Camera.vue
@@ -4,28 +4,58 @@ import axios from 'axios';
 import { API_HOST } from './components/Const';
 
 let pictureUrl = ref()
-let checked: boolean = false
+const tempAutoUpdate = localStorage.getItem("autoUpdateChecked")
+let autoUpdateChecked: boolean = (tempAutoUpdate == "true")
+const tempMovenetEnable = localStorage.getItem("movenetEnableChecked")
+let movenetEnableChecked: boolean = (tempMovenetEnable == "true")
 let timerId: number
 
 onMounted(() => {
 
     updatePicture()
 
+    if (autoUpdateChecked) {
+        timerId = setInterval(updatePicture, 1000, true)
+    }
+
 });
 
-function changed(item:Event) {
-    if (checked) {
+function autoUpdateChanged(item:Event) {
+    if (autoUpdateChecked) {
         timerId = setInterval(updatePicture, 1000, true)
     } else {
         clearInterval(timerId);
     }
+    localStorage.setItem("autoUpdateChecked", `${autoUpdateChecked}`)
+}
+
+function movenetEnableChanged(item:Event) {
+    localStorage.setItem("movenetEnableChecked", `${movenetEnableChecked}`)
 }
 
 function updatePicture() {
     axios.get(API_HOST + '/raspicameradata')
         .then(response => {
-            pictureUrl.value = 'data:image/png;base64,' + response.data.image;
-        });
+            if (movenetEnableChecked) {
+                execPostureEstimation(response.data.image)
+            } else {
+                pictureUrl.value = 'data:image/png;base64,' + response.data.image;
+            }
+        })
+        .catch(error => {
+            alert(error)
+        });        
+}
+
+function execPostureEstimation(base64img:string) {
+
+    axios.post(API_HOST + '/movenet/inference/', {image: base64img})
+        .then(response => {
+            pictureUrl.value = 'data:image/png;base64,' + btoa(response.data);
+        })
+        .catch(error => {
+            pictureUrl.value = 'data:image/png;base64,' + base64img;
+        })
 }
 
 </script>
@@ -35,8 +65,13 @@ function updatePicture() {
         <router-link to="/">Back To Top</router-link>
         <h2>RaspberryPi カメラ</h2>
         <div>
-            <input type="checkbox" id="autoUpdate" name="autoUpdate" v-model="checked" @change="changed">
+            <input type="checkbox" id="autoUpdate" name="autoUpdate" v-model="autoUpdateChecked" @change="autoUpdateChanged">
             <label for="autoUpdate">Auto Update</label>
+        </div>
+        <div>
+            <input type="checkbox" id="movenet" name="movenet" v-model="movenetEnableChecked" @change="movenetEnableChanged">
+            <label for="movenet">Posture estimation</label>
+
         </div>
         <img v-bind:src="pictureUrl" ref="img" />
     </main>
--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import Chart from 'chart.js/auto';
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, onBeforeUnmount } from 'vue';
 import { type Ref } from 'vue';
 import axios from 'axios';
 import moment from 'moment';
 import { API_HOST } from './Const';
 
+const count = ref()
+
+const tempHistoryCount = localStorage.getItem("tempHistoryCount")
+count.value = Number(tempHistoryCount) | 100
 const canvas1Ref = ref()
 const canvas2Ref = ref()
 
@@ -14,9 +18,25 @@ let chart2:Chart | undefined
 
 onMounted(() => {
     updateCharts()
+    window.addEventListener('resize', onResize)
 })
 
+onBeforeUnmount(() => {
+    window.removeEventListener('resize', onResize)
+})
+
+function onResize() {
+    if (chart1) {
+        chart1.update();
+    }
+    if (chart2) {
+        chart2.update();
+    }
+}
+
 function updateCharts() {
+
+    localStorage.setItem("tempHistoryCount", count.value)
 
     collectData().then((response) => {
 
@@ -62,7 +82,9 @@ function createCharts (chart:Chart | undefined, canvasRef:Ref, title:string, col
 
 async function collectData() : Promise<ApiResult> {
 
-    const { data } = await axios.get(API_HOST + '/sensorlogs')
+    const tmpCount:number = Number(count.value) || 100 
+
+    const { data } = await axios.get(API_HOST + `/sensorlogs?count=${tmpCount}`)
 
     let labels:string[] = []
     let data1:number[] = []
@@ -95,8 +117,19 @@ interface ApiResult {
 
 <template>
     <div>
+        <input v-model="count" placeholder="" type="number" min="100" max="10000" step="100">
         <button @click="updateCharts()">更新</button>
-        <canvas ref="canvas1Ref" style="width: 800px; height: 400px;" />
-        <canvas ref="canvas2Ref" style="width: 800px; height: 400px;" />
+        <canvas ref="canvas1Ref"  />
+        <canvas ref="canvas2Ref"  />
     </div>
 </template>
+
+<style module>
+
+canvas {
+    width: 90vw;
+    min-width: 800px;
+    max-height: 400px;
+}
+
+</style>
